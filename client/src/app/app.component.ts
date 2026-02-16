9d7b9c37739509a128ddf4d4844b812da921d913
Kyle - One Doc <kyle@onedocgroup.com>
2026-02-16 11:04:51 +0800

    use zoneless instead zone js

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
@@ -16,12 +16,12 @@ export class AppComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/';
   private http = inject(HttpClient);
    title = 'Skinet';
-  products: Product[] = [];
+  products = signal<Product[]>([]);
 
 
   ngOnInit(): void {
     this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products = response.data,
+      next: response => this.products.set(response.data),
       error: error => console.log(error),
       complete: () =>  console.log('complete')
     })
