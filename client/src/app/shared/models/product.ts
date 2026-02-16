40551dd5ebbb362757ce33379dceef0c6f683b7d
Kyle - One Doc <kyle@onedocgroup.com>
2026-02-16 10:43:18 +0800

    end of section 8

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,11 @@
+// or can using type but with = sign
+export interface Product {
+    id :number;
+    name: string;
+    description: string;
+    price: number;
+    prictureUrl: string;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+}
